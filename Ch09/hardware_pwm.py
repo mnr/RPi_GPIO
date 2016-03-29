3f3acb8525147582036fd08c2231269b3806291a
Mark Niemann-Ross <mnr@niemannross.com>
2016-03-29 15:42:45 -0700

    lynda compliant folder names

--- Ch09/hardware_pwm.py
+++ Ch09/hardware_pwm.py
@@ -4,7 +4,7 @@ from time import sleep
 wiringpi.wiringPiSetupGpio()
 wiringpi.pinMode(18,2) #bcm pin 18 is hardware pwm0
 
-for j in range(3):
+for j in range(200):
     for i in range(0,1024):
         wiringpi.pwmWrite(18,i)
         sleep(.001)
