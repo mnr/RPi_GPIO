fdde60dfc9906af4c8688106b0deff7ebed304d0
Mark Niemann-Ross <mnr@niemannross.com>
2016-04-04 16:25:44 -0700

    add try-except to trap out control-c

--- Ch08/08_03/Motor_driver_L293D_with_PWM.py
+++ Ch08/08_03/Motor_driver_L293D_with_PWM.py
@@ -21,20 +21,21 @@ def function PwmControl(self,turnForward):
             wiringpi.pwmWrite(MotorEnable,i)
             sleep(.001)
 
-for j in range(3):
-    print ("Turning Forward")
-    wiringpi.digitalWrite(MotorForward,1)
-    wiringpi.digitalWrite(MotorBackward,0)
-    PwmControl(True)
-    PwmControl(False)
-        
-    print ("Turning Backwards")
+try:
+    for j in range(3):
+        print ("Turning Forward")
+        wiringpi.digitalWrite(MotorForward,1)
+        wiringpi.digitalWrite(MotorBackward,0)
+        PwmControl(True)
+        PwmControl(False)
+            
+        print ("Turning Backwards")
+        wiringpi.digitalWrite(MotorForward,0)
+        wiringpi.digitalWrite(MotorBackward,1)
+        PwmControl(True)
+        PwmControl(False)
+except KeyboardInterrupt: #control-c
+    # cleanup the gpio
     wiringpi.digitalWrite(MotorForward,0)
-    wiringpi.digitalWrite(MotorBackward,1)
-    PwmControl(True)
-    PwmControl(False)
-
-# cleanup the gpio
-wiringpi.digitalWrite(MotorForward,0)
-wiringpi.digitalWrite(MotorBackward,0)
-wiringpi.pwmWrite(MotorEnable,0)
+    wiringpi.digitalWrite(MotorBackward,0)
+    wiringpi.pwmWrite(MotorEnable,0)
